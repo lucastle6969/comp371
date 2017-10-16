bdb62675d7ccd3e9c79864d55bb234acbf9dc409
Ben Wiley <therealbenwiley@gmail.com>
2017-10-15 20:19:23 -0400

    Obsolete includes removed.

--- pathloader.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PATHLOADER_H
-#define PATHLOADER_H
-
-#include <glm/glm.hpp>
-#include <vector>
-
-bool loadPath(const char* path, std::vector<glm::vec3>* vertices);
-
-#endif // PATHLOADER_H
