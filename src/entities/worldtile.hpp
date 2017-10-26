20c9f4e0bdf0ddf009c6eb721736c8dcf7e7b8b3
lucastle6969 <lucastle69@hotmail.com>
2017-10-26 11:46:40 -0400

    set base_rotation_angle to 0

--- src/entities/worldtile.hpp
+++ src/entities/worldtile.hpp
@@ -16,7 +16,7 @@
 
 class WorldTile: public DrawableEntity {
 private:
-    static constexpr float base_rotation_angle = (float)(M_PI / 2);
+    static constexpr float base_rotation_angle = 0;
     const glm::mat4& getBaseRotation() override;
     const glm::vec3& getDefaultFaceVector() override;
     std::vector<glm::vec3> vertices;
