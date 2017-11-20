0193d9df9f010fc55a323830886b3bd404017915
Ben Wiley <ben.wiley@autodesk.com>
2017-11-19 19:07:03 -0500

    Rock texture switched, bad-loading textures removed.

--- src/entities/Rock.cpp
+++ src/entities/Rock.cpp
@@ -489,7 +489,7 @@ GLuint Rock::getTextureId()
     //load image, create texture and generate mipmaps
     int rock_tex_width, rock_tex_height, rock_tex_nrChannels;
 
-	std::string path = "../textures/baliRockB.png";
+	std::string path = "../textures/baliRockB.jpg";
 	unsigned char* rock_tex_data = stbi_load(path.c_str(), &rock_tex_width, &rock_tex_height, &rock_tex_nrChannels, 0);
     if (rock_tex_data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, rock_tex_width, rock_tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, rock_tex_data);
