ab4da43150775391959f86de9977a1af517ffff7
kng <kai.nicoll.griffith@gmail.com>
2017-11-20 20:26:35 -0500

    New version in comments for manual merger

--- src/entities/Tree.cpp
+++ src/entities/Tree.cpp
@@ -62,3 +62,66 @@ GLuint Tree::getVAO() {
 const int Tree::getColorType() {
 	return COLOR_WHITE;
 }
+
+
+
+/*
+//
+// Created by Tof on 2017-11-19.
+//
+
+#include "Tree.hpp"
+
+
+glm::vec3 Tree::boostSegment(AttatchmentGroupings* agLow, AttatchmentGroupings* agHigh, std::vector<glm::vec3>* vPntr){
+    glm::vec3 AB = vPntr->at(agLow->end) - vPntr->at(agLow->end -1);
+    glm::vec3 AC = vPntr->at(agLow->end-2) - vPntr->at(agLow->end -1);
+    glm::vec3 low =  glm::normalize(glm::cross(AB, AC));
+    glm::vec3 KJ = vPntr->at(agHigh->start + 1) - vPntr->at(agHigh->start+2);
+    glm::vec3 KL = vPntr->at(agHigh->start + 3) - vPntr->at(agHigh->start+2);
+    glm::vec3 high =  -glm::normalize(glm::cross(KJ, KL));
+    return low + high;
+}
+
+//values in radians
+glm::vec3 Tree::makeRotations(float xRot, float yRot, float zRot, glm::vec3 vector){
+    //ROTATION ABOUT X
+    // std::cout<< xRot << " "<< yRot << " " << zRot <<"\n" << vector.x <<" " << vector.y << " " << vector.z << "\n";
+    vector = glm::vec3(
+            vector.x
+            , vector.y * cos(xRot) - vector.z * sin(xRot)
+            ,	vector.y * sin(xRot) + vector.z * cos(xRot));
+    //ROTATION ABOUT Z
+    // << xRot << " "<< yRot << " " << zRot <<"\n" << vector.x <<" " << vector.y << " " << vector.z << "\n";
+    vector = glm::vec3(
+            vector.x * cos(zRot) - vector.y * sin(zRot)
+            , vector.x * sin(zRot) + vector.y * cos(zRot)
+            ,	vector.z );
+    //ROTATION ABOUT Y
+    // << xRot << " "<< yRot << " " << zRot <<"\n" << vector.x <<" " << vector.y << " " << vector.z << "\n";
+    vector = glm::vec3(
+            vector.x * cos(yRot) + vector.z * sin(yRot)
+            , vector.y
+            ,	-(vector.x * sin(yRot)) + vector.z * cos(yRot));
+    // << xRot << " "<< yRot << " " << zRot <<"\n" << vector.x <<" " << vector.y << " " << vector.z << "\n-------\n";
+    return vector;
+}
+
+//TODO: COMPLETE CONECTORS
+void Tree::connectSegments(AttatchmentGroupings* ag, int m, int rotPoint, int prevPoint, int circularPoints,
+                     std::vector<GLuint>* indPntr){
+    int set = abs((circularPoints - rotPoint) + prevPoint);
+    TrunkA::buildConnectorElements(ag->end - TrunkA::trunkPoints + 1, ag->ag[m]->start + 1, set, ag->side,
+                                   combinedIndices, combinedVertices, combinedUV, combinedNormals);
+}
+
+void Tree::computeElementsInitial(AttatchmentGroupings* ag) {
+    if(ag->type == 'L'){
+        LeafContainerA::buildLeafContainerElements(ag->start + 1, ag->end,
+                                                   combinedIndices, combinedVertices, combinedUV, combinedNormals);
+    }
+    else
+        TrunkA::buildTrunkElements(ag->start + 1, ag->end,
+                                   combinedIndices, combinedVertices, combinedUV, combinedNormals);
+}
+*/
--- src/entities/Tree.hpp
+++ src/entities/Tree.hpp
@@ -45,3 +45,105 @@ public:
 
 
 #endif //PROCEDURALWORLD_TREE_HPP
+
+
+/*
+//
+// Created by Tof on 2017-11-19.
+//
+
+#ifndef PROCEDURALWORLD_TREE_H
+#define PROCEDURALWORLD_TREE_H
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+#else
+#include <GL/glew.h> // include GL Extension Wrangler
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+//container class
+
+#include "trunkA.hpp"
+#include "LeafContainerA.hpp"
+#include "entity.hpp"
+
+class Tree {
+private:
+protected:
+    class AttatchmentGroupings {
+    public:
+        //START END ANGLEX ANGLEY ANGLEZ
+        int start;
+        int end;
+        int angleX;
+        int angleY;
+        int angleZ;
+        char type;
+        char side;
+
+        AttatchmentGroupings* ag[2];
+
+    AttatchmentGroupings(int start,	int end, int angleX, int angleY, int angleZ, char type,	char side){
+        this->start = start;
+        this->end = end;
+        this->angleX = angleX;
+        this->angleY = angleY;
+        this->angleZ = angleZ;
+        this->type = type;
+        this->side = side;
+        ag[0] = nullptr; ag[1] = nullptr;
+    }
+    void selfErase() {
+        for (AttatchmentGroupings* agI: this->ag){
+            if (agI != nullptr) {
+                agI->selfErase();
+                delete agI;
+                agI = nullptr;
+            }
+        }
+    }
+};
+
+    std::vector<AttatchmentGroupings> branchStore;
+    std::vector<glm::vec3>* combinedVertices = new std::vector<glm::vec3>; 	std::vector<GLuint>* combinedIndices = new std::vector<GLuint>;		std::vector<glm::vec3>* combinedColor = new std::vector<glm::vec3>; std::vector<glm::vec3>* combinedNormals= new std::vector<glm::vec3>;
+    std::vector<glm::vec2>* combinedUV = new std::vector<glm::vec2>; std::vector<std::vector<int>> *combinedStartIndices = new std::vector<std::vector<int>> ;
+    GLuint vao; GLuint vbo; GLuint ebo; GLuint tbo;
+
+    float heightChunking;
+    int boostFactor;
+
+    float widthCut;
+    float finalCut;
+
+    Entity* e;
+
+public:
+    Tree(int heightChunking, float boostFactor){
+        this->heightChunking = heightChunking;
+        this->boostFactor = boostFactor;
+    }
+    Tree(const GLuint& shader_program, Entity* entity, float trunkDiameter, float seed){};
+    ~Tree(){
+        delete combinedVertices; 	delete combinedIndices ;	delete combinedColor; delete combinedNormals;
+        delete combinedUV ; delete combinedStartIndices;
+    }
+
+    glm::vec3 boostSegment(AttatchmentGroupings* agLow, AttatchmentGroupings* agHigh, std::vector<glm::vec3>* vPntr);
+
+    //values in radians
+    glm::vec3 makeRotations(float xRot, float yRot, float zRot, glm::vec3 vector);
+
+    void connectSegments(AttatchmentGroupings* ag, int m, int rotPoint, int prevPoint, int circularPoints,
+                         std::vector<GLuint>* indPntr);
+
+    void computeElementsInitial(AttatchmentGroupings* ag);
+};
+
+
+#endif //PROCEDURALWORLD_TREE_H
+*/
