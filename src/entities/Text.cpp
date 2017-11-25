fc368bc39f920a2f768bbc41ea718b9e07b72ed3
lucastle6969 <lucastle69@hotmail.com>
2017-11-24 22:33:12 -0500

    Changed Vertices

--- src/entities/Text.cpp
+++ src/entities/Text.cpp
@@ -39,9 +39,9 @@ Text::~Text()
     {
         static const std::vector<glm::vec3> vertices = {
                 glm::vec3(0.0f, 0.0f, 0.0f),
-                glm::vec3(2.0f, 0.0f, 0.0f),
-                glm::vec3(0.0f, 2.0f, 0.0f),
-                glm::vec3(2.0f, 2.0f, 0.0f)
+                glm::vec3(1.0f, 0.0f, 0.0f),
+                glm::vec3(0.0f, 1.0f, 0.0f),
+                glm::vec3(1.0f, 1.0f, 0.0f)
         };
         return vertices;
     }
